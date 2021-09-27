a97288b52b0d1c61bace0316976d5d976c3a2553
Merlin Osayimwen <merlinosayimwen@gmail.com>
2021-09-27 02:10:02 +0200

    Improve noscript and page files

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -26,7 +26,6 @@ installThemes().then(() => {
 	);
 })
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals().catch(details => {
+  console.log({error: 'failed to report web vitals', details})
+})
